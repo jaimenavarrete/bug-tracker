ba623bf36ee0fa3b72169f5c64ee9b4eaf1637ff
Jaime Navarrete <jaimenava123@outlook.com>
2023-02-08 12:09:55 -0600

    Soluciona problema con botones para abrir y cerrar sidebar

--- src/Frontend/Angular/src/app/shared/components/sidebar/sidebar.component.ts
+++ src/Frontend/Angular/src/app/shared/components/sidebar/sidebar.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input } from '@angular/core';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.scss']
+  styleUrls: ['./sidebar.component.scss'],
 })
 export class SidebarComponent {
+  @Input() sidebarOpen!: boolean;
 
+  constructor() {}
+
+  changeSidebarState() {
+    this.sidebarOpen = !this.sidebarOpen;
+  }
 }
--- src/Frontend/Angular/src/app/shared/components/topbar/topbar.component.ts
+++ src/Frontend/Angular/src/app/shared/components/topbar/topbar.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-topbar',
   templateUrl: './topbar.component.html',
-  styleUrls: ['./topbar.component.scss']
+  styleUrls: ['./topbar.component.scss'],
 })
 export class TopbarComponent {
+  sidebarOpen: boolean;
+  @Output() sidebarOpenClick: EventEmitter<boolean>;
 
+  constructor() {
+    this.sidebarOpen = true;
+    this.sidebarOpenClick = new EventEmitter<boolean>();
+  }
+
+  changeSidebarState() {
+    this.sidebarOpen = !this.sidebarOpen;
+
+    this.sidebarOpenClick.emit(this.sidebarOpen);
+  }
 }
