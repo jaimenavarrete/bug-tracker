6193ea712796de969a51a9ca77dc049f80e50c1f
Jaime Navarrete <jaimenava123@outlook.com>
2023-02-12 01:18:09 -0600

    Agrega listado de proyectos en el sidebar

--- src/Frontend/Angular/src/app/shared/components/sidebar/sidebar.component.ts
+++ src/Frontend/Angular/src/app/shared/components/sidebar/sidebar.component.ts
@@ -1,14 +1,26 @@
-import { Component, EventEmitter, Input } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit } from '@angular/core';
+import { tap } from 'rxjs';
+import { ProjectList } from 'src/app/pages/projects/interfaces/project-list.interface';
+import { ProjectsService } from 'src/app/pages/projects/services/projects.service';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.scss'],
 })
-export class SidebarComponent {
+export class SidebarComponent implements OnInit {
+  projectsList!: ProjectList[];
   @Input() sidebarOpen!: boolean;
 
-  constructor() {}
+  constructor(private projectsService: ProjectsService) {}
+
+  ngOnInit(): void {
+    this.projectsService.projectsListAction$.subscribe(
+      (res) => (this.projectsList = res.data)
+    );
+
+    this.projectsService.projectsListSubjectUpdate();
+  }
 
   changeSidebarState() {
     this.sidebarOpen = !this.sidebarOpen;
