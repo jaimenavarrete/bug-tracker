c2e60e25168e46caafabe3a884d1a456474c1cd5
Jaime Navarrete <jaimenava123@outlook.com>
2023-02-17 12:10:56 -0600

    Agrega funcionalidad de etiquetas a form de creación de proyectos
    Permite agregar y eliminar etiquetas del proyecto.
    Agrega las etiquetas de proyecto escritas directamente en el código.

--- src/Frontend/Angular/src/app/pages/projects/components/create-project-modal/create-project-modal.component.ts
+++ src/Frontend/Angular/src/app/pages/projects/components/create-project-modal/create-project-modal.component.ts
@@ -1,11 +1,4 @@
-import {
-  Component,
-  ElementRef,
-  EventEmitter,
-  OnInit,
-  Output,
-  ViewChild,
-} from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { tap } from 'rxjs';
 import { CreateProject } from '../../interfaces/create-project.interface';
@@ -27,8 +20,22 @@ export class CreateProjectModalComponent implements OnInit {
 
   groupsList!: GroupList[];
   projectStatesList!: ProjectStateList[];
+  projectTagsList = [
+    {
+      id: 'ee84a39d-06bf-44a7-bf3b-1930a10945a6',
+      name: 'Tag1',
+      colorHexCode: '#FF0000',
+    },
+    {
+      id: 'c17a00b9-3aa1-4518-b1ce-6a46a456f1c1',
+      name: 'Tag2',
+      colorHexCode: '#00FF00',
+    },
+  ];
 
   model!: CreateProject;
+  inputTagName = '';
+  assignedTagsList!: { id: string; name: string; colorHexCode: string }[];
   isLoading: boolean = false;
 
   constructor(
@@ -44,10 +51,36 @@ export class CreateProjectModalComponent implements OnInit {
     this.getProjectStates();
   }
 
-  onSubmit({ value: formData, valid: isValid }: NgForm) {
+  addAssignedTag(event: KeyboardEvent): void {
+    if (event.key === ',') {
+      let tagName = this.inputTagName.replaceAll(',', '').trim();
+      tagName = tagName.toLowerCase();
+
+      let newTag = this.projectTagsList.find(
+        (tag) => tag.name.toLowerCase() === tagName
+      );
+
+      if (newTag) {
+        this.inputTagName = '';
+        this.model.assignedTagsId?.push(newTag.id);
+        this.assignedTagsList?.push(newTag);
+      }
+    }
+  }
+
+  removeAssignedTag(assignedTagId: string): void {
+    let tagIndex = this.model.assignedTagsId?.indexOf(assignedTagId);
+
+    if (tagIndex !== undefined && tagIndex !== -1) {
+      this.model.assignedTagsId?.splice(tagIndex, 1);
+      this.assignedTagsList?.splice(tagIndex, 1);
+    }
+  }
+
+  onSubmit({ valid: isValid }: NgForm) {
     if (isValid) {
       this.isLoading = true;
-      this.createProject(formData);
+      this.createProject(this.model);
     } else {
       alert('The form is invalid');
     }
@@ -63,8 +96,11 @@ export class CreateProjectModalComponent implements OnInit {
       completionDate: undefined,
       stateId: '',
       groupId: '',
-      assignedTagsId: undefined,
+      assignedTagsId: [],
     };
+
+    this.inputTagName = '';
+    this.assignedTagsList = [];
   }
 
   private getGroups(): void {
